76a9345a37aa1c781f1ce1571081b2f7a8c474cd
ChairulR <148623628+ChairulR@users.noreply.github.com>
2024-09-09 09:45:47 +0700

    Tugas 3 MTK

--- src/Tugas3/Matematika.java
+++ src/Tugas3/Matematika.java
@@ -1,5 +1,16 @@
 package Tugas3;
 
 public class Matematika {
-    
+    public double pertambahan(double a,double b){
+        return a + b;
+    }
+    public double pertambahan(double a,double b,double c){
+        return a + b + c;
+    }
+    public double pengurangan (double a,double b){
+        return a + b;
+    }
+    public double pengurangan (double a,double b,double c){
+        return a + b + c;
+    }
 }
