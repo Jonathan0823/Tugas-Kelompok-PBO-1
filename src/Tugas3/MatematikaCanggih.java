e509f8ca57c6f7e1f82a83b81892d3a3c0af2627
ChairulR <148623628+ChairulR@users.noreply.github.com>
2024-09-09 09:49:49 +0700

    MTK Canggih

--- src/Tugas3/MatematikaCanggih.java
+++ src/Tugas3/MatematikaCanggih.java
@@ -1,5 +1,20 @@
 package Tugas3;
 
-public class MatematikaCanggih {
+public class MatematikaCanggih extends Matematika {
+    String nama;
+    String nim;
     
+    public MatematikaCanggih(String nama,String nim){
+    this.nama = nama;
+    this.nim = nim;
+    }
+    public double perkalian (double a, double b){
+       return a * b; 
+    }
+    public double pembagian (double a, double b){
+       return a / b;
+    }
+    public double modulus (double a, double b){
+       return a % b;
+    }
 }
