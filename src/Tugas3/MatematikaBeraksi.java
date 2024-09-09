4f4da4b73f4dd32e16a017a0045ddf57a43489a8
ChairulR <148623628+ChairulR@users.noreply.github.com>
2024-09-09 09:47:43 +0700

    Tugas MTK Beraksi

--- src/Tugas3/MatematikaBeraksi.java
+++ src/Tugas3/MatematikaBeraksi.java
@@ -1,5 +1,18 @@
 package Tugas3;
 
 public class MatematikaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MatematikaCanggih mtk = new MatematikaCanggih("Chairul", "23100");
+        System.out.println("Pertambahan" + mtk.pertambahan(12.5, 28.7, 14.2));
+        System.out.println("Pertambahan" + mtk.pertambahan(23, 34.5));
+        System.out.println("Pertambahan" + mtk.pertambahan(12.6, 28, 14));
+        System.out.println("Pertambahan" + mtk.pertambahan(3.4, 9));
+        
+        
+    }
     
 }
