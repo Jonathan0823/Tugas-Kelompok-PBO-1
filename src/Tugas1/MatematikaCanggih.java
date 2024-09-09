b9aa61809aec53562ac638619a3239d96559f3d3
Denisha RF <67154111+drfdenden@users.noreply.github.com>
2024-09-10 00:34:32 +0700

    MatematikaCanggih.java

--- src/Tugas1/MatematikaCanggih.java
+++ src/Tugas1/MatematikaCanggih.java
@@ -1,19 +1,33 @@
 package Tugas1;
 
 public class MatematikaCanggih extends Matematika {
-    public double pertambahan(double a, double b) {
-        return a + b;
-    } 
+    String NIM;
+    String Nama;
     
-    public double pertambahan(double a, double b, double c) {
-        return a + b + c;
+    public MatematikaCanggih(String NIM, String Nama) {
+        this.NIM = NIM;
+        this.Nama = Nama;
     }
     
-    public double pengurangan(double a, double b) {
-        return a - b;
+    public double perkalian(double a, double b) {
+        return a * b;
     }
     
-    public double pengurangan(double a, double b, double c) {
-        return a - b - c;
+    public double pembagian(double a, double b) {
+        if(b != 0) {
+            return a / b;
+        }else {
+            System.out.println("Pembagian dengan nol tidak diperbolehkan");
+            return 0;
+        }
+    }
+    public double modulus(double a, double b) {
+        return a % b;
+    }        
+    
+    public void tampilkanData() {
+        System.out.println("NIM     : " +NIM);
+        System.out.println("Nama    : " +Nama);
+   
     }
 }
