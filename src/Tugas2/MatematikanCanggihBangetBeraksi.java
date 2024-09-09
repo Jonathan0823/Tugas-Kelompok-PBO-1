7c427007bd0bbb7722641d3fed2be923db4736c3
Denisha RF <67154111+drfdenden@users.noreply.github.com>
2024-09-10 00:42:36 +0700

    Update MatematikanCanggihBangetBeraksi.java

--- src/Tugas2/MatematikanCanggihBangetBeraksi.java
+++ src/Tugas2/MatematikanCanggihBangetBeraksi.java
@@ -1,5 +1,14 @@
 package Tugas2;
 
 public class MatematikanCanggihBangetBeraksi {
-    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MatematikaCanggihBanget mtk = new MatematikaCanggihBanget();
+        
+        System.out.println("Hasil pertambahan yaitu " +mtk.tambah(20, 10));
+        System.out.println("Hasil perkalian yaitu " +mtk.kali(20, 10));
+        System.out.println("Hasil modulus yaitu " +mtk.modulus(20, 10));
+        System.out.println("Hasil pertambahan tiga yaitu " +mtk.pertambahanTiga(20, 10, 15));
+        
+    }
 }
