79a881f0c430d1561301c0cc537caf7d650b9b2b
Denisha RF <67154111+drfdenden@users.noreply.github.com>
2024-09-10 00:38:28 +0700

    create MatematikaCanggih.java

---
+++ src/Tugas2/MatematikaCanggih.java
@@ -0,0 +1,28 @@
+package tugas2;
+  
+public class MatematikaCanggih {
+    public int tambah(int a, int b) {
+        return a + b;
+    }
+    
+    public int kurang(int a, int b) {
+        return a - b;
+    }
+    
+    public int kali(int a, int b) {
+        return a * b;
+    }
+    
+    public int bagi(int a, int b) {
+        if(b != 0) {
+            return a / b;
+        }else {
+            System.out.println("Pembagian dengan nol tidak diperbolehkan");
+            return 0;
+        }
+    }
+    
+    public int modulus(int a, int b) {
+        return a % b;
+    }
+}
