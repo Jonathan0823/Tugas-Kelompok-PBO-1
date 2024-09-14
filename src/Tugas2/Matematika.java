58edc0c0cca7f15597df70c7130abae3d447b877
Guinn <eguinjonathan08@gmail.com>
2024-09-14 13:00:25 +0700

    changed the method location to match the assignments

--- src/Tugas2/Matematika.java
+++ src/Tugas2/Matematika.java
@@ -16,4 +16,17 @@ public class Matematika {
     public double pengurangan(double a, double b, double c) {
         return a - b - c;
     }
+
+    public double perkalian(double a, double b) {
+        return a * b;
+    }
+    
+    public double pembagian(double a, double b) {
+        if(b != 0) {
+            return a / b;
+        }else {
+            System.out.println("Pembagian dengan nol tidak diperbolehkan");
+            return 0;
+        }
+    }
 }
--- src/Tugas2/MatematikaCanggih.java
+++ src/Tugas2/MatematikaCanggih.java
@@ -9,18 +9,7 @@ public class MatematikaCanggih extends Matematika {
         this.Nama = Nama;
     }
     
-    public double perkalian(double a, double b) {
-        return a * b;
-    }
     
-    public double pembagian(double a, double b) {
-        if(b != 0) {
-            return a / b;
-        }else {
-            System.out.println("Pembagian dengan nol tidak diperbolehkan");
-            return 0;
-        }
-    }
     public double modulus(double a, double b) {
         return a % b;
     }        
